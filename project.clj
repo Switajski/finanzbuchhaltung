a3ab6bc1eae817b65c6503057a1eda86453349ba
Marek <marek@switajski.de>
2019-06-05 12:10:29 +0200

    Use clojure.data.json to format JSON stream

--- project.clj
+++ project.clj
@@ -6,11 +6,12 @@
                  [compojure "1.6.1"]
                  [ring/ring-json "0.3.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [org.clojure/data.xml "0.0.8"]]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler de.switajski.finanzbuchhaltung.handler/app
-         :port 4000
-         :nrepl {:start? true}}
+         :port    4000
+         :nrepl   {:start? true}}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
--- src/de/switajski/finanzbuchhaltung/handler.clj
+++ src/de/switajski/finanzbuchhaltung/handler.clj
@@ -5,37 +5,35 @@
             [compojure.route :as route]
             [de.switajski.dbf :as dbf]
             [clojure.java.io :as io]
+            [clojure.data.json :as json]
             [ring.util.io :as ring-io])
   (:import [java.io BufferedInputStream FileInputStream]))
 
 (def BUFFER-SIZE 8192)
 
-(defn format-line [rec fields]
-  (apply str
-         (for [field fields]
-           (format "\"%s\":\"%s\"," (name field) (field rec)))))
-
 (defn stream!
-  [in-file fields & {:keys [conv encoding]
-                     :or   {conv {} encoding "UTF8"}}]
+  [in-file & {:keys [conv encoding]
+              :or   {conv {} encoding "UTF8"}}]
   (ring-io/piped-input-stream
-   #(let [writer (io/make-writer % {})
-          dbf-meta (dbf/read-dbf-meta in-file)
-          dbf (BufferedInputStream. (FileInputStream. ^String in-file)
-                                    BUFFER-SIZE)]
-      (doseq [rec (dbf/read-records! dbf dbf-meta conv)]
-        (when (not (:deleted rec))
-          (.write writer (format "{%s}\n" (format-line rec fields)))
-          (.flush writer))))))
+    #(let [writer (io/make-writer % {})
+           dbf-meta (dbf/read-dbf-meta in-file)
+           dbf (BufferedInputStream. (FileInputStream. ^String in-file)
+                                     BUFFER-SIZE)]
+       (.write writer "[{}")
+       (doseq [rec (dbf/read-records! dbf dbf-meta conv)]
+         (when (not (:deleted rec))
+           (.write writer ",")
+           (json/write rec writer)))
+       (.write writer "]")
+       (.flush writer))))
 
 (defroutes app-routes
-  (GET "/" [] {:status 200
-               :body   (stream!
-                        "/tmp/buchen.dbf"
-                        [:art :dat :konto :gegen :btext :rech_nr :datensatz :betrag_h :betrag_s :vmsteuer :bdatum :konto_art :klnummer :faellig :kklasse :zamek :bilg]
-                        :fields {:kdse :chars-to-int
-                                 :kse  :chars-to-int})})
-  (route/not-found "Not Found"))
+           (GET "/api" [] {:status 200
+                           :body   (stream!
+                                     "/tmp/buchen.dbf"
+                                     :fields {:kdse :chars-to-int
+                                              :kse  :chars-to-int})})
+           (route/not-found "Not Found"))
 
 (def app
   (-> (handler/site app-routes)
