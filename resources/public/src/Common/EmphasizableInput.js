6e6a8f9e36aaa86e8d9fbd1686a313bee391762d
Marek <marek@switajski.de>
2019-12-05 07:46:54 +0100

    clean up for releases

--- resources/public/src/Common/EmphasizableInput.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-import LabeledInput from './LabeledInput'
-
-const EmphasizableInput = styled(LabeledInput)`
-${props => props.emphasize && 'background-color:' + props.theme.emphasize + ';'}`
-
-export default EmphasizableInput
--- resources/public/src/TODO.md
+++
@@ -1,7 +0,0 @@
-# TODOs
-
-make fa08.dbf instead of taxes.edn
-
-✔︎ update record is not working
-
-are there tax account on activa/passiva. When yes, do I have to create datensatz B differently, then?
