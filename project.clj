9e7cf2b32291507356eb960be249c778216700aa
Marek <marek@switajski.de>
2019-12-31 08:14:32 +0100

    configure automatic release

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Simple accounting web application using DBF files"
   :url "https://finanzbuchhaltung.switajski.de"
   :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
@@ -10,7 +12,9 @@
                  [compojure "1.6.1"]
                  [nl.knaw.dans.common/dans-dbf-lib "1.0.0-beta-10"]
                  [org.clojure/data.json "0.2.6"]]
-  :plugins [[lein-ring "0.12.5"]]
+  :scm {:name "git" :url "https://github.com/switajski/finanzbuchhaltung"}
+  :plugins [[lein-ring "0.12.5"]
+            [lein-s3-uberjar-release "0.1.0"]]
   :main de.switajski.fibu.handler
   :ring {:handler de.switajski.fibu.handler/app
          :port    4000
@@ -18,5 +22,9 @@
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}}
+  :s3-uberjar-release {:bucket "public-switajski"
+                       :prefix "/releases"                  ; %s is current git hash
+                       ;:filename "raflui-standalone.jar"
+                       }
   ;:jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5010"] ;for debugging https://stackoverflow.com/questions/56300204/how-to-debug-a-clojure-web-application-in-intellij
   )
---
+++ resources/public/precache-manifest.08a1dccc48a69442a0b9c57b4eb6e7ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893c676d714b51e200665004587b892f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "268ea08b4ca14eef5fcf",
+    "url": "/static/css/main.20c46d02.chunk.css"
+  },
+  {
+    "revision": "59daac7e11e22a18c5ce",
+    "url": "/static/js/2.90a7900e.chunk.js"
+  },
+  {
+    "revision": "268ea08b4ca14eef5fcf",
+    "url": "/static/js/main.8a659439.chunk.js"
+  },
+  {
+    "revision": "a85570faef596aa7a246",
+    "url": "/static/js/runtime-main.1a291d17.js"
+  }
+]);
