88ec650cdec0fdf9831356fd07caddcb49e000b4
Marek <marek@switajski.de>
2019-12-31 13:06:24 +0100

    fix UI build

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject de.switajski.finanzbuchhaltung "2.1.5"
+(defproject de.switajski.finanzbuchhaltung "2.1.6-SNAPSHOT"
   :description "Simple accounting web application using DBF files"
   :url "https://finanzbuchhaltung.switajski.de"
   :min-lein-version "2.0.0"
